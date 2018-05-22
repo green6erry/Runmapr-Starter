38f1dc011196fd1bd139d65fc87ca9e9bdcc2941
green6erry <green6erry@gmail.com>
2018-05-22 11:30:00 -0400

    Initial Commit

---
+++ Rumapr-Starter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rumapr-Starter
+//
+//  Created by Amanda Brown on 5/22/18.
+//  Copyright © 2018 AGB Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
